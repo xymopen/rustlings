18672f0bf88251b4875e047a91f8e2be5b1154cd
xymopen <xuyiming.open@outlook.com>
2019-10-14 15:58:07 +0800

    Execrise 45

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -9,9 +9,13 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
+
+// pub fn capitalize_first(input: &str) -> String {
+
+// }
 
 #[cfg(test)]
 mod tests {
@@ -33,14 +37,19 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = words.iter()
+            .map(|word| capitalize_first(word))
+            .collect::<Vec<String>>();
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = words.iter()
+            .map(|word| capitalize_first(word))
+            .collect::<Vec<String>>()
+            .join("");
         assert_eq!(capitalized_words, "Hello World");
     }
 }
