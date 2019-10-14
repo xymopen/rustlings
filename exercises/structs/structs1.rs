62545c432d298216ef4d3cc3c5d2d50dd1ae3270
xymopen <xuyiming.open@outlook.com>
2019-10-14 10:46:45 +0800

    Execrise 18

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,10 +2,11 @@
 // Address all the TODOs to make the tests pass!
 
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    name: &'static str,
+    hex: &'static str,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(&'static str, &'static str);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -16,8 +17,11 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
+        // Instantiate a classic c struct!
+        let green = ColorClassicStruct {
+            name: "green",
+            hex: "#00FF00",
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -25,9 +29,9 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
+        // Instantiate a tuple struct!
         // For more fun, use the field initialization shorthand.
-        // let green =
+        let green = ColorTupleStruct("green", "#00FF00");
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -35,8 +39,8 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        // Instantiate a unit struct!
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
