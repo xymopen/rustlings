8cf3c0f25a885ab5a4434d1c30d6a650af19a9ec
xymopen <xuyiming.open@outlook.com>
2019-10-14 15:38:12 +0800

    Execrise 44

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -9,13 +9,15 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
+        let cloned_numbers = Arc::clone(&shared_numbers);
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
+            let child_numbers = cloned_numbers;
             while i < child_numbers.len() {
                 sum += child_numbers[i];
                 i += 5;
